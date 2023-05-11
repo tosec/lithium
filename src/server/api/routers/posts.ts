2d8b1379bb580720a1734143e59b5f340c6ffdd6
tosec <stephenvallejo44@gmail.com>
2023-05-11 10:09:03 -0400

    minor css, render posts & user deatils for posts, && dayjs

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,46 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUser = (user: User) => {
+  return {
+    id: user.id,
+    firstName: user.firstName,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUser);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author || !author.firstName)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Author not found",
+        });
+      return {
+        post,
+        author: {
+          ...author, 
+          firstName: author.firstName
+        },
+      };
+    });
   }),
 });
